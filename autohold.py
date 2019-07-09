f91d913119e2b6aa3c4faf604f721ab2f57f541f
pronect <shock4236@gmail.com>
2019-07-10 00:52:13 +0500

    Автосписание суммы холда из баланаса клиента с последующим очищением холда.

---
+++ autohold.py
@@ -0,0 +1,25 @@
+import time
+from app import db
+from app.models.user import User
+
+# Получаем список id всех клиентов
+session = db.session
+lst = session.query(User.id).all()
+ids = []
+for id in lst:
+    ids.append(id[0])
+
+
+# Вычет суммы hold из баланса клиента
+def subhold():
+    for id in ids:
+        select = User.query.filter_by(id=id).first()
+        if (select.balance - select.hold) > 0 and select.status is True:
+            select.balance = select.balance - select.hold
+            select.hold = 0
+            db.session.commit()
+
+
+while True:
+    time.sleep(10)
+    subhold()
