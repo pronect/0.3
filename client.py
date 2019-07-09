a9b7bb50ca3a4b0a8bbec9d542d2a7e3b9731891
pronect <shock4236@gmail.com>
2019-07-10 00:53:17 +0500

    довлен autohold.py Автосписание суммы холда из баланаса клиента с последующим очищением холда.

--- client.py
+++
@@ -1,6 +0,0 @@
-from jsonrpcclient import request
-
-ping = request("http://localhost:5000/", "ping").data.result
-print(ping)
-status = request("http://localhost:5000/", "status").data.result
-print(status)
