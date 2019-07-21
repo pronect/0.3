a438e5ed3b98f472f457984a43097d7d654f6cc2
pronect <shock4236@gmail.com>
2019-07-21 15:30:16 +0500

    изменен импорт

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -6,84 +6,81 @@ from app.models.user import User
 
 @app.route('/api/ping', methods=['POST'])
 def ping():
-    data = {
-        'status': 'http статус запроса',
-        'result': 'true',
-        'addition': 'none',
-        'description': 'Работоспособность сервиса',
-    }
-    return jsonify(data)
+    return request.form
 
 
 @app.route('/api/add', methods=['POST'])
 def add():
     client_id = request.form.get('client_id')
-    money = int(request.form.get('money'))
+    money = float(request.form.get('money'))
     select = User.query.filter_by(id=client_id).first()
-    data = {
-        'status': '200',
-        'result': 'true',
-        'addition': {
-            'id:': select.id,
-            'ФИО:': select.name,
-            'Баланс:': select.balance,
-            'Статус счета:': select.status,
-        },
-        'description': 'пополнение баланса',
-    }
-    if select.status:
-        select.balance += money
+    exists = db.session.query(User.id).filter_by(id=client_id).scalar() is not None
+    if exists and select.status:
+        data = {
+            'status': '200',
+            'result': 'true',
+            'addition': {
+                'id:': select.id,
+                'ФИО:': select.name,
+                'Текущий баланс:': select.balance,
+                'Статус счета:': select.status,
+                'Сумма операции:': money,
+            },
+            'description': 'Пополнение баланса',
+        }
+        select.balance = select.balance + money
         db.session.commit()
         return jsonify(data)
-    else:
-        return jsonify({'счет закрыт': 'none'})
+    return jsonify({'description': 'Счет закрыт или пользователь не существует'})
 
 
 @app.route('/api/substruct', methods=['POST'])
 def substract():
     client_id = request.form.get('client_id')
-    money = request.form.get('money')
+    money = float(request.form.get('money'))
     select = User.query.filter_by(id=client_id).first()
-    result = select.balance - select.hold - int(money)
-    if result > 0:
-        select.balance -= int(money)
-        db.session.commit()
-        data = {
-            'status': '<200>',
-            'result': 'true',
-            'addition': {
-                'id:': select.id,
-                'ФИО:': select.name,
-                'Баланс:': select.balance,
-                'Статус счета:': select.status,
-            },
-            'description': 'уменьшение баланса',
-        }
-        return jsonify(data)
-    else:
-        return jsonify({
-            'none': 'операция не возможна',
-            'select': select.balance,
-            'hold': select.hold,
-            'операция': money})
+    exists = db.session.query(User.id).filter_by(id=client_id).scalar() is not None
+    if exists:
+        result = select.balance - select.hold - money
+        if result > 0 and select.status is True:
+            select.hold = select.hold + money
+            db.session.commit()
+            data = {
+                'status': '200',
+                'result': 'true',
+                'addition': {
+                    'id:': select.id,
+                    'ФИО:': select.name,
+                    'Баланс:': select.balance,
+                    'Статус счета:': select.status,
+                    'Сумма операции:': money,
+                },
+                'description': 'Уменьшение баланса',
+            }
+            return jsonify(data)
+    return jsonify({'description': 'Счет закрыт или пользователь не существует'})
 
 
 @app.route('/api/status', methods=['POST'])
 def status():
     client_id = request.form.get('client_id')
-    if client_id:
+    exists = db.session.query(User.id).filter_by(id=client_id).scalar() is not None
+    if exists and client_id:
         select = User.query.filter_by(id=client_id).first()
         data = {
-            'status': '<http_status>',
-            'result': '<bool:operation_status>',
-            'addition': (select.balance, select.status),
-            'description': 'остаток по балансу, открыт счет или закрыт',
+            'status': '200',
+            'result': 'true',
+            'addition': {
+                'Баланс:': select.balance,
+                'Статус счета:': select.status
+            },
+            'description': 'Остаток по балансу, открыт счет или закрыт',
         }
         return jsonify(data)
-    else:
-        return jsonify({'None': 'None'})
+    return jsonify({'info': 'Не указан id клиента, либо пользваотель не существует'})
 
 
+# добавляет нового клиента в базу
 @app.route('/api/add_user', methods=['POST'])
 def add_user():
     client_id = request.form.get('client_id')
--- app/run.py
+++ app/run.py
@@ -1,16 +1,12 @@
 from app import app
 import app.controllers.controller as api
-
-api.ping
-api.add
-api.substract
-api.status
-api.add_user
+from app.controllers.controller import app
 
 
 @app.route('/')
 def welcome():
     return """service is alive"""
+
 
 if __name__ == "__main__":
     app.run(debug=True)
