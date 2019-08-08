7ad2688f63429f082cff383664105527bc08ae29
pronect <shock4236@gmail.com>
2019-08-08 21:31:23 +0500

    wrong url response json

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,68 +1,90 @@
 from app import app, db
 from app.models.user import User
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, json, make_response
 
 
-@app.route('/api/test', methods=['POST'])
-def api_format_source():
-    request.get_json()
-    print(request.get_json())
-    if request.json is None:
-        abort(400)
-    return jsonify(request.get_json())
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
 
 
+def addition(id):
+    """шаблон для json. возвращает всю строку из базы"""
+    select = User.query.filter_by(id=id).first()
+    return {
+        "id": select.id,
+        "name": select.name,
+        "balance": select.balance,
+        "hold": select.hold,
+        "state": select.status,
+    }
+
+
+# def response_template(id):
+#     select = User.query.filter_by(id=id).first()
+#     return select.name
+#
+#
+# @app.route('/api/test', methods=['POST'])
+# def api_format_source():
+#     request.get_json()
+#     if request.get_json() is None:
+#         abort(400)
+#     return jsonify(request.get_json())
+
+# для тестов
 @app.route('/api/test2', methods=['POST'])
 def api():
+    d = make_response()
     data = request.get_json()
-    print(data)
-    select = User.query.filter_by(id=data['client_id']).first()
-    data2 = {
-        'id': select.id,
-        'name': select.name
+    response = app.response_class(
+        response=json.dumps(data),
+        status=400,
+        mimetype='application/json'
+    )
+    response = jsonify(data)
+    res = response.status_code = 200
+    data = {
+        "status": res
     }
-    return jsonify(data2)
+    return (jsonify(data))
 
 
 @app.route('/api/ping', methods=['POST'])
 def ping():
-    fields = [k for k in request.form]
-    values = [request.form[k] for k in request.form]
-    data = dict(zip(fields, values))
+    data = request.get_json()
     return jsonify(data)
 
 
 @app.route('/api/add', methods=['POST'])
 def add():
-    client_id = request.form.get('client_id')
-    money = float(request.form.get('money'))
-    select = User.query.filter_by(id=client_id).first()
-    exists = db.session.query(User.id).filter_by(id=client_id).scalar() is not None
+    data = request.get_json()
+    id = data['id']
+    money = data['money']
+    money = round(float(money), 2)
+    select = User.query.filter_by(id=id).first()
+    exists = db.session.query(User.id).filter_by(id=id).scalar() is not None
     if exists and select.status:
+        select.balance = select.balance + money
+        db.session.commit()
         data = {
             'status': '200',
             'result': 'true',
-            'addition': {
-                'id:': select.id,
-                'ФИО:': select.name,
-                'Текущий баланс:': select.balance,
-                'Статус счета:': select.status,
-                'Сумма операции:': money,
-            },
-            'description': 'Пополнение баланса',
+            'addition': addition(id),
+            'description': 'add balance',
         }
-        select.balance = select.balance + money
-        db.session.commit()
         return jsonify(data)
     return jsonify({'description': 'Счет закрыт или пользователь не существует'})
 
 
-@app.route('/api/substruct', methods=['POST'])
+@app.route('/api/subtract', methods=['POST'])
 def substract():
-    client_id = request.form.get('client_id')
-    money = float(request.form.get('money'))
-    select = User.query.filter_by(id=client_id).first()
-    exists = db.session.query(User.id).filter_by(id=client_id).scalar() is not None
+    data = request.get_json()
+    id = data['id']
+    money = data['money']
+    money = round(float(money), 2)
+    select = User.query.filter_by(id=id).first()
+    exists = db.session.query(User.id).filter_by(id=id).scalar() is not None
     result = select.balance - select.hold - money
     if exists and result > 0 and select.status is True:
         select.hold = select.hold + money
@@ -70,14 +92,8 @@ def substract():
         data = {
             'status': '200',
             'result': 'true',
-            'addition': {
-                'id:': select.id,
-                'ФИО:': select.name,
-                'Баланс:': select.balance,
-                'Статус счета:': select.status,
-                'Сумма операции:': money,
-            },
-            'description': 'Уменьшение баланса',
+            'addition': addition(id),
+            'description': 'subtract balance',
         }
         return jsonify(data)
     return jsonify({'description': 'Счет закрыт или пользователь не существует'})
@@ -85,17 +101,14 @@ def substract():
 
 @app.route('/api/status', methods=['POST'])
 def status():
-    client_id = request.form.get('client_id')
-    exists = db.session.query(User.id).filter_by(id=client_id).scalar() is not None
-    if exists and client_id:
-        select = User.query.filter_by(id=client_id).first()
+    data = request.get_json()
+    id = data['id']
+    exists = db.session.query(User.id).filter_by(id=id).scalar() is not None
+    if exists and id:
         data = {
             'status': '200',
             'result': 'true',
-            'addition': {
-                'Баланс:': select.balance,
-                'Статус счета:': select.status
-            },
+            'addition': addition(id),
             'description': 'Остаток по балансу, открыт счет или закрыт',
         }
         return jsonify(data)
